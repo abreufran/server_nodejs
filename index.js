af6d099fab4a629f68d3d281d3d7371ccd61da39
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2021-05-25 13:49:17 -0400

    Se creo un metodo para recibir request de tipo POST

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -7,7 +9,11 @@ const db = mysql.createPool({
     user: 'root',
     password: '123456',
     database: 'crud_react'
-})
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     const sqlInsert = "Insert into crud_react.movie_reviews (movieName, movieReview) VALUES('Al filo del mañana', 'Buena película')";
@@ -19,6 +25,20 @@ app.get('/', (req, res) => {
     });
     
 })
+
+app.post('/api/insert', (req, res) => {
+
+    const movieName = req.body.movieName
+    const movieReview = req.body.movieReview
+
+    const sqlInsert = "Insert into crud_react.movie_reviews (movieName, movieReview) VALUES(?, ?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(result);
+    })
+});
 
 app.listen(3001, () => {
     console.log("running on port 3001");
